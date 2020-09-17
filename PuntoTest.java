c080e870a0c5b20e74b259bc7929711951b73c80
agusdeba <agusdebattista@gmail.com>
2020-09-17 08:37:00 -0300

    Primer commit

---
+++ PuntoTest.java
@@ -0,0 +1,8 @@
+package Proyecto_1;
+
+public class PuntoTest {
+	public static void main(String [] args) {
+		Punto p1 = new Punto(2,4);
+		System.out.println("Este es el punto: "+p1.toString());
+	}
+}
