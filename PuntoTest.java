fc6eb849b7ba57ad7fbe83d08515e26d262e4167
agusdeba <agusdebattista@gmail.com>
2020-09-17 09:17:21 -0300

    Resolviendo conflicto

--- PuntoTest.java
+++ PuntoTest.java
@@ -3,6 +3,6 @@ package Proyecto_1;
 public class PuntoTest {
 	public static void main(String [] args) {
 		Punto p1 = new Punto(2,4);
-		System.out.println("Este es el punto: "+p1.toString());
+		System.out.println("Punto: "+p1.toString());
 	}
 }
