f115ac8174c070fb012ab4ae85496b200cf337bb
agusdeba <agusdebattista@gmail.com>
2020-09-17 09:00:16 -0300

    Agregando modificacion del clone

--- PuntoTest.java
+++ PuntoTest.java
@@ -3,6 +3,6 @@ package Proyecto_1;
 public class PuntoTest {
 	public static void main(String [] args) {
 		Punto p1 = new Punto(2,4);
-		System.out.println("Este es el punto: "+p1.toString());
+		System.out.println("Hola, este es el punto creado: "+p1.toString());
 	}
 }
